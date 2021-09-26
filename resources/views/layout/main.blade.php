deb7160f9153c37d868ac29ef933b38601c0fed7
Rionaldo Tio <51085902+Reddemon1@users.noreply.github.com>
2021-09-26 22:45:35 +0700

    p

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -13,8 +13,8 @@
 
         <title>Hello, world!</title>
     </head>
-    <body>
-            <nav style="height:60px;" class="navbar navbar-expand-lg navbar-light bg-light nav">
+    <body >
+            <nav style="height:60px;" class="navbar navbar-expand-lg fixed-top navbar-light bg-light nav">
                 <div class="container-fluid" style="margin-left: 100px">
                 <a style="margin-top:0" class="navbar-brand"  href="#"><img width="42" height="42" src="image/logo.png" alt=""></a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
